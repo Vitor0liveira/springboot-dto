952a539d5a4b9cf9d198dc05af72674cc96d9948
Vitor0liveira <vitoroliveira.ads@gmail.com>
2022-01-09 16:24:07 -0300

    Entity to DTO Conversion using ModelMapper Library

--- src/main/java/com/jv/example/springbootdtotutorial/SpringbootDtoTutorialApplication.java
+++ src/main/java/com/jv/example/springbootdtotutorial/SpringbootDtoTutorialApplication.java
@@ -4,13 +4,20 @@ import com.jv.example.springbootdtotutorial.model.Location;
 import com.jv.example.springbootdtotutorial.model.User;
 import com.jv.example.springbootdtotutorial.repository.LocationRepository;
 import com.jv.example.springbootdtotutorial.repository.UserRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootDtoTutorialApplication implements CommandLineRunner {
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDtoTutorialApplication.class, args);
--- src/main/java/com/jv/example/springbootdtotutorial/service/UserService.java
+++ src/main/java/com/jv/example/springbootdtotutorial/service/UserService.java
@@ -4,7 +4,8 @@ import com.jv.example.springbootdtotutorial.dto.UserLocationDTO;
 import com.jv.example.springbootdtotutorial.model.User;
 import com.jv.example.springbootdtotutorial.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,6 +16,7 @@ import java.util.stream.Collectors;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
 
     public List<UserLocationDTO> getAllUsersLocations(){
         return userRepository.findAll()
@@ -23,15 +25,30 @@ public class UserService {
                 .collect(Collectors.toList());
     }
 
+//    private UserLocationDTO convertEntityToDto(User user){
+//        UserLocationDTO userLocationDTO = new UserLocationDTO();
+//
+//        userLocationDTO.setUserId(user.getId());
+//        userLocationDTO.setEmail(user.getEmail());
+//        userLocationDTO.setPlace(user.getLocation().getPlace());
+//        userLocationDTO.setLongitude(user.getLocation().getLongitude());
+//        userLocationDTO.setLatitude(user.getLocation().getLatitude());
+//
+//        return  userLocationDTO;
+//    }
     private UserLocationDTO convertEntityToDto(User user){
+        modelMapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.LOOSE);
         UserLocationDTO userLocationDTO = new UserLocationDTO();
-
-        userLocationDTO.setUserId(user.getId());
-        userLocationDTO.setEmail(user.getEmail());
-        userLocationDTO.setPlace(user.getLocation().getPlace());
-        userLocationDTO.setLongitude(user.getLocation().getLongitude());
-        userLocationDTO.setLatitude(user.getLocation().getLatitude());
-
+        userLocationDTO = modelMapper.map(user, UserLocationDTO.class);
         return  userLocationDTO;
+    }
+
+    private User convertDtoToEntity(UserLocationDTO userLocationDTO){
+        modelMapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.LOOSE);
+        User user = new User();
+        user = modelMapper.map(userLocationDTO, User.class);
+        return user;
     }
 }
